eebc54773358b110e36cbfdbd13a96ab5149d6e5
Tracylu2015 <89538089+Tracylu2015@users.noreply.github.com>
2022-01-28 15:32:33 -0800

    Update ALLOWED_HOSTS

--- news_stream_django/news_stream_django/settings.py
+++ news_stream_django/news_stream_django/settings.py
@@ -22,9 +22,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6tqw1vtu+)#6bh%c@6jpc*rv-p82)ucho6#+82#!)=t&r%i)jv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    '192.168.31.31',
+    'news-stream.spookyai.com'
+]
 
 # Application definition
 
