043de1641678368527d85434f04c9d9c71455eb8
Tracylu2015 <89538089+Tracylu2015@users.noreply.github.com>
2022-02-01 14:59:22 -0800

    insert to es

--- news_stream_streams/streams/kafka_main.py
+++ news_stream_streams/streams/kafka_main.py
@@ -1,17 +1,34 @@
 import json
+import logging
+import os
 
 from models.parse_json_to_model import parse_twitter_stream
 from prometheus_client import start_http_server
 from streams.kafka_consumer import KafkaConsumer
+from elasticsearch import Elasticsearch
+from datetime import datetime
+
+es = Elasticsearch(
+    [{"host": os.getenv('ELASTIC_HOST')}]
+)
 
 
 def start_consumer():
+    logging.basicConfig(level=logging.WARN)
     consumer = KafkaConsumer()
     topics = ["twitter_stream"]
-    data = {}
     for msg in consumer.basic_consume_loop(topics):
         # read from kafka server and insert into MongoDB
-        parse_twitter_stream(json.loads(msg.value().decode('utf-8'))).save()
+        post = parse_twitter_stream(json.loads(msg.value().decode('utf-8')))
+        post.save()
+        es_doc = json.loads(post.to_json())
+        if '_id' in es_doc:
+            del es_doc['_id']
+        es.index(
+            index="tstream-post-{suffix}".format(suffix=datetime.today().strftime('%Y.%m.%d')),
+            document=es_doc,
+            id=es_doc.get('post_id')
+        )
 
     consumer.shutdown()
 
